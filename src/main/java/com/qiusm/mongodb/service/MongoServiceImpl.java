b10de41b84f5b990fcb27983c391b2cefc7d398e
qiushengming <qiushengming@aliyun.com>
2022-05-09 14:28:16 +0800

    mongo 数据采用bulkOps进行批量插入。可以达到1500w/h的速度。

--- src/main/java/com/qiusm/mongodb/service/MongoServiceImpl.java
+++ src/main/java/com/qiusm/mongodb/service/MongoServiceImpl.java
@@ -4,19 +4,30 @@ import com.alibaba.fastjson.JSONObject;
 import com.qiusm.spring.service.MongoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.data.mongodb.core.BulkOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author qiushengming
  */
 @Slf4j
 @DubboService(version = "0.0.1")
-public class MongoServiceImpl implements MongoService {
+public class MongoServiceImpl implements MongoService, DisposableBean {
 
     @Resource
     private MongoTemplate mongoTemplate;
+
+    private final List<JSONObject> cacheList = new ArrayList<>(10000);
+
+    private static final Boolean LOCK = true;
+    final static private Integer UPPER_LIMIT = 9000;
+
+    private Integer count = 0;
 
     /**
      * 保存挂号登记信息
@@ -26,13 +37,28 @@ public class MongoServiceImpl implements MongoService {
      */
     @Override
     public Boolean insert(String s, String collectionName) {
+        count++;
         try {
             JSONObject jsonobj = JSONObject.parseObject(s);
-            mongoTemplate.insert(jsonobj, collectionName);
+            cacheList.add(jsonobj);
+            if (cacheList.size() > UPPER_LIMIT) {
+                synchronized (LOCK) {
+                    // mongoTemplate.insert(cacheList, collectionName);
+                    BulkOperations bulkOps = mongoTemplate.bulkOps(BulkOperations.BulkMode.UNORDERED, collectionName);
+                    bulkOps.insert(cacheList);
+                    cacheList.clear();
+                    log.info("已经处理数量：{}", count);
+                }
+            }
+
             return true;
         } catch (Exception e) {
             log.error("params: {}, error_message: {}", s, e.getMessage());
             return false;
         }
+    }
+
+    @Override
+    public void destroy() throws Exception {
     }
 }
